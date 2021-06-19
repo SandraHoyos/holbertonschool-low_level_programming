5f382cee5b6a97f45c39f1353fb5f7c6710983f8
SandraHoyos <slhoyosr@gmail.com>
2021-06-18 18:59:12 -0700

    Primer programa c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
+/*
+ * File: 4-puts-c
+ * Sandra Lorena Hoyos
+ */
+
 #include <stdio.h>
 
 /*
- * main - Entry point
+ * main - Prints a message on screen
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+  
   puts("\"Programming is like building a multilingual puzzle");
 
   return (0);
+  
 }

99d7dccc8ce64d84645222e715ed74422b5983ab
SandraHoyos <slhoyosr@gmail.com>
2021-06-18 18:46:53 -0700

    Primer programa c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+  puts("Programming is like a multilingual puzzle \n");
+
+  return (0)
+    }

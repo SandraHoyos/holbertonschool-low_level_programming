f6e7c773097cc8fa0122634b2c70e8171f1e4d57
SandraHoyos <slhoyosr@gmail.com>
2021-06-18 19:42:11 -0700

    Puts en c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,15 @@
  * Sandra Lorena Hoyos
  */
 #include <stdio.h>
-/**
+/*
  *int main(void)- Prints a message on screen
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
+/**
+ * main - Función principal del programa
+ */
+void main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
 }

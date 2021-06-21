f264a314550ded57d7af8ee5a0f2a825c66a8f68
SandraHoyos <slhoyosr@gmail.com>
2021-06-20 21:27:53 -0700

    ahora si?

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ * File: 5-printf.c
+ * Sandra Lorena Hoyos
+ */
+#include <stdio.h>
+
+/**
+ * main - Función principal del programa que imprime un mensaje en pantalla
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}

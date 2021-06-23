faad49b0e900e829b41974f47dbcd605bcc09335
SandraHoyos <slhoyosr@gmail.com>
2021-06-22 21:35:55 -0500

    For for Holberton

---
+++ holbertonschool-low_level_programming/0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Prints "Holberton\n" string
+ *
+ *
+ * Return: On success 0.
+ *
+ */
+int main(void)
+{
+
+  char cadena[10]="Holberton\n";
+  for(int i=0;i<=10;i++)
+    {
+      _putchar(cadena[i]);
+    }
+  return (0)
+}

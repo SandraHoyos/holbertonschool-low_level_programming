03ee1ec6ece8a26945f65396e0ef704ef4a0be07
root <root@Portatil.localdomain>
2021-06-24 00:40:20 -0500

    funcion que imprime el signo de un numero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 
 #include "holberton.h"
 /**
- * _sign - funcion that prints of a number
+ * print _sign - funcion that prints of a number
  * @n:caracter to check
  *
  *
@@ -14,16 +14,16 @@ if ( n < 0)
 {
 _putchar('-');
  
- return(-1);
- }
-else if (n == 0);
- {
-   _putchar('0');
+return(-1);
+}
+else if (n == 0)
+{
+_putchar('0');
 return(0);
- }
-if ( n > 0);
- {
-   _putchar('+');
-  return(1);
+}
+if ( n > 0)
+{
+_putchar('+');
+return(1);
 }
 }

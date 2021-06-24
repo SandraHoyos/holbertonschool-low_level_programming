c226a64904ff074f9e83162a9af1da36bf6a3ae6
root <root@Portatil.localdomain>
2021-06-24 15:01:31 -0500

    imprimir signo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,12 @@
  */
 int print_sign(int n)
 {
-if ( n < 0 )
+if (n < 0)
 {
 _putchar(45);
 return(-1);
 }
-else if ( n == 0 )
+else if (n == 0)
 {
 _putchar(48);
 return(0);
@@ -24,4 +24,4 @@ else
 _putchar(43);
 return(1);
 }
- }
+}

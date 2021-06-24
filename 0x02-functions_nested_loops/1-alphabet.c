ce82ceea5040eb659fb06dfa8b126368f60ae737
root <root@Portatil.localdomain>
2021-06-23 21:21:28 -0500

    alfabeto

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Prints "alphabet\n" string
+ *
+ *
+ * Return: On success 0.
+ *
+ */
+int main(void)
+{
+  char letra;
+  for (letra = 'a'; letra <= 'z'; letra++)
+    {
+      _putchar(letra);
+    }
+  return (0);
+}

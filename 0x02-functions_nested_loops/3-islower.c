2acd23df8c76e57e9c8314eb11f7089f910f0047
root <root@Portatil.localdomain>
2021-06-23 23:05:45 -0500

    caracteres en minusculas

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * int _islower\n" string
+ *
+ *
+ * Return: int.
+ *
+ */
+int _islowerintc(int c); #include "holberton.h"
+
+
+{
+if (c >= 'a' && c<= 'z')
+return (1);
+else
+return (0);
+}

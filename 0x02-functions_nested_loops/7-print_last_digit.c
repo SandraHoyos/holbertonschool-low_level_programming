d8722511d90d4295b7909046afd3f3f1e984f56b
root <root@Portatil.localdomain>
2021-06-24 22:48:51 -0500

    imprime el ultimo digito

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,17 @@
 /**
 *
 * print_last_digit - function that prints the last digit of a number
-* @numero last digit
+* @numero: last digit
 *
 * return (n)
 */
 int print_last_digit(int numero)
 {
-  int i, p;
+int i, p;
 i= numero % 10;
 if (i < 0)
 i = -i;
-p = '0' +i;
+p = '0' + i;
 _putchar (p);
 return (i);
 }

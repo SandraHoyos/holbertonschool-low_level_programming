901309c7c279ca87f4362f9cb0ad2309c16171b5
root <root@Portatil.localdomain>
2021-06-24 22:31:41 -0500

    ultimo digito

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberto.h"
+/**
+*
+* print_last_digit - function that prints the last digit of a number
+* @numero last digit
+*
+* return (n)
+*/
+int print_last_digit(int numero)
+{
+int numero;
+i= numero % 10;
+if (i < 0)
+i = -i;
+p = '0' +i;
+_putchar(p);
+retur(i);
+}
+
+
+
+

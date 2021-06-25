9fc976c6418ff4f11e049bf9c80d54ac4814cd1e
root <root@Portatil.localdomain>
2021-06-24 22:53:23 -0500

    imprime el ultimo digito

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
+
 /**
-*
-* print_last_digit - function that prints the last digit of a number
-* @numero: last digit
-*
-* return (n)
-*/
+ *
+ * print_last_digit - function that prints the last digit of a number
+ * @numero: last digit
+ *
+ * return (n)
+ */
 int print_last_digit(int numero)
 {
 int i, p;
-i= numero % 10;
+i = numero % 10;
 if (i < 0)
 i = -i;
 p = '0' + i;

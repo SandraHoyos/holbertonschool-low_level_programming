fa9d2f57ee4b12d7d88b8db3bf6a2dda97571506
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 00:11:56 -0500

    adition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-#
+#include "holberton.h"
+/**
+ *
+ * add - this is a function that add two integer numbers
+ *
+ * @n1: number to add
+ * @n2: number to add
+ * Return: adition of n1 + n2
+ */
+
+int add (int n1, int n2)
+{
+  return (n1 + n2);
+}

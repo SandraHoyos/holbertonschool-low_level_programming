be53a4a1c04193a779aa06a8a77339c3eb64cd6c
SandraHoyos <slhoyosr@gmail.com>
2021-06-24 23:59:30 -0500

    24'1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 
 /**
- *
  * jack_bauer - function that print Jack Bauer every minute
  *
  * Return: void
@@ -9,19 +8,18 @@
 
 void jack_bauer(void)
 {
-  int horas, minutos;
+int horas, minutos;
 
-  for (horas = 0; horas <= 23; horas++)
-    {
-      for (minutos = 0; minutos <= 59; minutos++)
-	{
-	  _putchar((horas / 10) + '0');
-	  _putchar((horas % 10) + '0');
-	  _putchar(':');
-	  _putchar((minutos / 10) + '0');
-	  _putchar((minutos % 10) + '0');
-	  _putchar('\n');
-	}
-
-    }
+for (horas = 0; horas <= 23; horas++)
+{
+for (minutos = 0; minutos <= 59; minutos++)
+{
+_putchar((horas / 10) + '0');
+_putchar((horas % 10) + '0');
+_putchar(':');
+_putchar((minutos / 10) + '0');
+_putchar((minutos % 10) + '0');
+_putchar('\n');
+}
+}
 }

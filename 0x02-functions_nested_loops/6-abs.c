a3901341327bcaf14e123790a75055982a988f52
root <root@Portatil.localdomain>
2021-06-24 21:22:44 -0500

    retorna un numero con valor absoluto

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* int _abs(int) - function the absolute value.
+* @numero
+*
+* Return: n
+*/
+int _abs(int numero)
+{
+if (numero < 0)
+{
+numero*=-1;
+return(numero);     
+}
+else
+{
+return(numero):
+}
+}

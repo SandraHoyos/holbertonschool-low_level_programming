741cf308e5b4cd9ecbe40ad350207c76173365b8
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 21:37:13 -0500

    multiplicación

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,8 @@
 /**
  * mul - Multiplica dos enteros
  *  variable caracter para revisar si es digito
- *
+ *@a: entero 1 para multiplicar
+ *@b: entero 2 para multiplicar
  * Return: multiplicación de a y b
  */
 int mul(int a, int b)

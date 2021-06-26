7a93e649f1fd22794fb3b6860742daf7331841e6
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 21:50:43 -0500

    imprime números

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
  * print_numbers - imprime los numeros del 0 al 9
- * 
+ * @i: indice del for
  * Return: void
  */
 void print_numbers(void)
 {
 int i;
 for (i = 0;i <= 9; i++)
+{
 _putchar(i);
+}
 _putchar("\n");
 }

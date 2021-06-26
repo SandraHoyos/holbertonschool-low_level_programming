776bc4ca2c225c2f0963ec2ddf16a695b62aa8c7
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 21:49:02 -0500

    imprime números

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_numbers - imprime los numeros del 0 al 9
+ * 
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0;i <= 9; i++)
+_putchar(i);
+_putchar("\n");
+}

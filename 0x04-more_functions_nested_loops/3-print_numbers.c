dc1f4cbbab8c38d64f2ad72f409139df32c38a13
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 22:54:16 -0500

    hobie

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 /**
  * print_numbers - imprime los numeros 0-9.
+ *
+ *@n entero que da los c+odigos ascii para putchar
+ *
+ *Return: void
  */
 void print_numbers(void)
 {
 int n;
-for (n = 48; n < 58 ; n++)
+for (n = 48; n <= 57 ; n++)
 {  
 _putchar(n);
 }

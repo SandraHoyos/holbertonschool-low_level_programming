7ce314dc1cc01ef569f3b0dc531d227a513148d9
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 23:27:18 -0500

    ni 2 ni 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
  */
@@ -8,8 +7,10 @@ void print_most_numbers(void)
 int n;
 for (n = 48; n <= 57 ; n++)
 {
-  if (!(n =50 || n = 52))
+  if (n != 50 || n != 52)
+{
 _putchar(n);
+}
 }
 _putchar('\n');
 }

5f24fefaef1e3143806767cb7c643cb84d64f80f
SandraHoyos <slhoyosr@gmail.com>
2021-06-25 21:32:31 -0500

    multiplicación

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - Multiplica dos enteros
+ *  variable caracter para revisar si es digito
+ *
+ * Return: void
+ */
+int mul(int a, int b)
+{
+printf ( "%d\n", a*b);
+}

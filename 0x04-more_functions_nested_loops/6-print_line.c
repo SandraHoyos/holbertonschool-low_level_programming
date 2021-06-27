89e9a5865039913dfd3e8f244a03aa77455c4aae
SandraHoyos <slhoyosr@gmail.com>
2021-06-26 22:21:53 -0500

    Linea

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line - prints a line 
+ *
+ * @n variable tamaño de linea
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+if (n >= 0)
+{
+for (i = 0;i <= n;i++)
+_putchar(95);
+}
+ _putchar('\n');
+}
+  

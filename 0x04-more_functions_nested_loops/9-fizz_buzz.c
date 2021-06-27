7418ed85ff38b3b6ec35a0fe6b0805ede8004187
SandraHoyos <slhoyosr@gmail.com>
2021-06-27 16:39:42 -0500

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "holberton.h"
-
 /**
  * main - Programa that prints the numbers from 1 to 100
  *
@@ -10,21 +9,20 @@
  */
 int main(void)
 {
-  int i;
-
-  for (i = 1; i <= 100; i++)
-    {
-      if (i % 15 == 0)
-	printf("FizzBuzz");
-      else if (i % 3 == 0)
-	printf("Fizz");
-      else if (i % 5 == 0)
-	printf("Buzz");
-      else
-	printf("%i", i);
-      if (i < 100)
-	printf(" ");
-    }
-  printf("\n");
-  return (0);
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%i", i);
+if (i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
 }

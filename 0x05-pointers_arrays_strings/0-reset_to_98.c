0d5ff34a0b16671baf846965c05a7b8714cf4108
root <root@Portatil.localdomain>
2021-06-29 22:46:26 -0500

    funcion que resetea puntero a 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "Holberton.h"
+
+/**
+ * reset_to_98 - function tht tekes a pointer to an integer and uddates the value to 98
+ *
+ * @n: int  pointer
+ *
+ * Return: void 
+ */
+void reset_to_98(int *n)
+{
+int *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+**Apuntadores**
+en este proyecto vamos a ver que son los punteros, como utilizarlos
+que son arrays, diferencias entre pnteros y matrices, como utilizar las cadenas y como manipularlas.

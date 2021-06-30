c34c41a169f308fda16a9500373d972b3dd018b3
root <root@Portatil.localdomain>
2021-06-29 23:53:00 -0500

    funcion que intercambia dos valores

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * swap_int - funtion that values of two integers
+ *
+ * @a, @b, @c; variables to exchange
+ *
+ * Return: void
+ */
+swap_int(int *a, int *b)
+{
+int c;
+c = a*;
+*a = *b;
+*b = c;
+}

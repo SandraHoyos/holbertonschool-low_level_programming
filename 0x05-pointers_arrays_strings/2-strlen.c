d2aabddc8c6dd3c08a4b2504b4a9c8b5514984fe
root <root@Portatil.localdomain>
2021-06-30 00:42:47 -0500

    funcion que devuelve la longitud de una cadena

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - function the returns the length of s tring
+ *
+ * @s: char variable to find length
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+  Return strlen(char s);
+}
+

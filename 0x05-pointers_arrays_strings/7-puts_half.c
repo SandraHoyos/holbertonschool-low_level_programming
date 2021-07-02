3a53ae0c8d45b3df8687a5a337e00611fb3d045a
root <root@Portatil.localdomain>
2021-07-02 00:45:41 -0500

    7 puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,12 +5,12 @@
  */
 void puts_half(char *str)
 {
-int i = 0, mitad = 0, j = 0;
+int i = 1, mitad = 0, j = 1;
 
 while (str[i] != '\0')
 i++;
 if (i % 2 == 0)
-mitad = (i - 1) / 2;
+mitad = i / 2;
 else
 mitad = (i + 1) / 2;
 for (j = mitad; j <= i; j++)

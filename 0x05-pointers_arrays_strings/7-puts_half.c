30f67f6366330ebdbd206377923194a7a67bedd7
root <root@Portatil.localdomain>
2021-07-02 00:27:11 -0500

    7 puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,8 +9,11 @@ int i = 0, mitad = 0, j = 0;
 
 while (str[i] != '\0')
 i++;
+if (i % 2 == 0)
 mitad = (i - 1) / 2;
-
+else
+mitad = (i + 1) / 2;
 for (j = mitad; j <= i; j++)
 putchar(str[j]);
+putchar('\n'),
 }

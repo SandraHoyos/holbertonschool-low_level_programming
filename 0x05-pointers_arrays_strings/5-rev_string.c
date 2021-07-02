9c5efa8bd513dc6b0349fe8aada9f9021c5088ba
root <root@Portatil.localdomain>
2021-07-01 21:49:33 -0500

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * rev_string - function that reverses a string
+ *@s: char to evaluate
+ *
+ * Return void
+ */
+void rev_string(char *s)
+{
+int lon, i, j = 0;
+char cadena[100];
+
+for (lon = 0; s[lon] != '\0'; lon++)
+{
+}
+
+for (i = lon; i >= 0; i--)
+{
+cadena[j] = s[i];
+j++;
+}
+for (i = 0; i <= lon; i++)
+{
+s[i] = cadena[i];
+putchar(s[i]);
+}
+}

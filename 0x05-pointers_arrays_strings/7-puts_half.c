3021d6ea980462347da3a8227eb43b476b1e5c71
root <root@Portatil.localdomain>
2021-07-02 00:12:52 -0500

    7 puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - Prints out every other char of the string.
+ * @str: input string to print.
+ */
+void puts2(char *str)
+{
+  int i = 0, mitad = 0, j = 0;
+
+while (str[i] != '\0')
+i++;
+mitad = (i - 1) / 2;
+
+for(j = mitad; j <= i; j++)
+putchar(str[j]);
+}

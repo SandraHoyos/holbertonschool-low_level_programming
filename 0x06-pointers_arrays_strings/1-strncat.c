cff40df772ec0939a6cd021337b8cc8d687c99a1
SandraHoyos <slhoyosr@gmail.com>
2021-07-02 23:05:56 -0500

    concatena cadenas

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,21 +8,19 @@
  *
  * Return: dest
  **/
-
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
+int i, j, k;
 
-i = 0;
-
+i = 0, j = 0;
 while (dest[i] != '\0')
 i++;
-for (j = 0; j < n; j++)
-{
-dest[i] = src[j];
-i++;
-}
-if (dest[i - 1] != '\0')
-dest[i] = '\0';
+while (src[j] != '\0')
+j++;
+if (n < j)
+j = n - 1;
+for (k = 0; k <= j; k++, i++)
+dest[i] = src[k];
+dest[i + 1] = '\0';
 return (dest);
 }

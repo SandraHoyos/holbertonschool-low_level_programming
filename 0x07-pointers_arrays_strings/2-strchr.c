c64208ff5322d02f7c0143b596f5653702d5e297
SandraHoyos <slhoyosr@gmail.com>
2021-07-06 21:07:33 -0500

    Busca caracter

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _strchr - Retorna un puntero a la primera ocurrencia
+*
+* @c: Caracter a buscar
+* @s: Cadena donde buscar el caracter
+*
+* Return : pointer or null
+*/
+
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0';i++)
+{
+if (s[i] == c)
+return (i);
+}
+return (NULL);
+}

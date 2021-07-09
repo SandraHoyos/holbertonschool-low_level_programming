51e498845a119d7f2042962abc9c3a9fda911fd6
SandraHoyos <slhoyosr@gmail.com>
2021-07-08 22:34:38 -0500

    Raiz cuadrada

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * sqrt_prueba - Evalua de 1 -n 
+ * @a: mismo numero que n
+ * @b: numero que itera de 1 a n
+ *
+ * Return: exitoso 1.
+ * Error, retorna -1.
+ */
+int sqrt_prueba(int a, int b)
+{
+if (b * b == a)
+return (b);
+else if (b * b > a)
+return (-1);
+return (sqrt_prueba(a, b + 1));
+}
+/**
+ * _sqrt_recursion - retorna la raiz cuadrada de n
+ * @n: Número entero
+ *
+ * Return: Exitoso 1.
+ * Error, retorna -1
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt_prueba(n, 1));
+}

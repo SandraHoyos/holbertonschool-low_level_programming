5cc17d515159f212f599a1d8fc7a1c44c9803510
SandraHoyos <slhoyosr@gmail.com>
2021-07-08 22:24:01 -0500

    Potencia

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - calcula el valor de x elevado a la y
+ * @x: Entero para calcular la potencia
+ * @y: Entero a elevar x
+ * Return: x * x a la potencia y-1;
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+return ( _pow_recursion(x, y - 1) * x);
+}

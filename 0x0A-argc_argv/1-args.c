433906276a4f8b3b6502b8e97eccb0e8ddd0f135
SandraHoyos <slhoyosr@gmail.com>
2021-07-09 23:35:11 -0500

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Imprime el numero de parametros
+ * @argc: número de argumentos
+ * @argv: arreglo de argumentos
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}

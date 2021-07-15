9e016406e4035c4c05dbd88e4df97210decfb6c1
SandraHoyos <slhoyosr@gmail.com>
2021-07-14 23:55:46 -0500

    Fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,22 +2,27 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- *main - multiplica dos n+umero
- *@argc: números
- *@argv: Argumentos
- *Return: 0
+ * main - Entry Point
+ * atoi is a function that converts a string into an int
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-int a, b, c;
+int i, res = 1;
 if (argc != 3)
 {
-puts("Error");
+printf("%s\n", "Error");
 return (1);
 }
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-c = a * b;
-printf("%d\n", c);
+else
+{
+for (i = 1; i < argc; i++)
+{
+res *= atoi(argv[i]);
+}
+printf("%d\n", res);
+}
 return (0);
 }

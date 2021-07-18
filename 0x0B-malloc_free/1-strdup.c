321caf39bcc872e8ea0980a8d80f18f52e53716c
SandraHoyos <slhoyosr@gmail.com>
2021-07-18 17:06:31 -0500

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,13 +10,13 @@
 char *_strdup(char *str)
 {
 char *s;
- int i, j;
+int i, j;
 if (str == 0)
 return (NULL);
 for (i = 0; str[i] != '\0'; i++)
 ;
 i++;
-s = malloc(i * sizeof(*s));
+s = malloc((i) * (sizeof(*s)));
 if (s == 0)
 return (NULL);
 for (j = 0; j < i; j++)

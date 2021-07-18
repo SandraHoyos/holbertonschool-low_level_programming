8138bc4565fbe1a59b86003cebd2d30700d3e94e
SandraHoyos <slhoyosr@gmail.com>
2021-07-18 17:03:39 -0500

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - function to return pointer containing copy of string
+ * @str: string to be copied
+ *
+ * Return: Null for failure or pointer.
+ */
+char *_strdup(char *str)
+{
+char *s;
+ int i, j;
+if (str == 0)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+;
+i++;
+s = malloc(i * sizeof(*s));
+if (s == 0)
+return (NULL);
+for (j = 0; j < i; j++)
+s[j] = str[j];
+return (s);
+}

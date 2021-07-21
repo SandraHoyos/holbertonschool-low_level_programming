6b3c5fd9fa938ae2e95812f1d71cda0e84944183
SandraHoyos <slhoyosr@gmail.com>
2021-07-20 23:57:59 -0500

    perro nuevo

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+
 int namelen = 0, ownerlen = 0, i = 0;
 dog_t *perro;
 
@@ -20,19 +21,15 @@ perro = malloc(sizeof(dog_t));
 if (perro == NULL)
 return (NULL);
 
-perro->name = malloc(namelen * sizeof(char));
+perro->name = malloc(namelen * sizeof(perro->name));
 if (perro == NULL)
-{
-free(perro);
 return (NULL);
-}
 for (i = 0; i < namelen; i++)
 perro->name[i] = name[i];
 perro->age = age;
 
-perro->owner = malloc(ownerlen * sizeof(char));
+perro->owner = malloc(ownerlen * sizeof(perro->owner));
 if (perro == NULL)
-free(perro);
 return (NULL);
 for (i = 0; i < ownerlen; i++)
 perro->owner[i] = owner[i];

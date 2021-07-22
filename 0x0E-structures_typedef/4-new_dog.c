80102d4ca7559b1a166ac9e49815b8d9d738030e
root <root@Portatil.localdomain>
2021-07-21 22:51:14 -0500

    fix checker 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,37 +9,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *new_d;
+int i;
 
-int namelen = 0, ownerlen = 0, i = 0;
-dog_t *perro;
-while (name[namelen++])
-;
-while (owner[ownerlen++])
-;
-perro = malloc(sizeof(dog_t));
-if (perro == NULL)
+new_d = malloc(sizeof(struct dog));
+if (new_d == NULL)
 return (NULL);
-free(perro);
-
-perro->name = malloc(namelen * sizeof(perro->name));
-if (perro->name == NULL)
+for (i = 0; name[i] != '\0'; i++)
+;
+new_d->name = malloc(sizeof(char) * i + 1);
+if (new_d->name == NULL)
 {
-return (NULL);
-free(perro->name);
-
- }
-for (i = 0; i < namelen; i++)
-perro->name[i] = name[i];
-perro->age = age;
-
-perro->owner = malloc(ownerlen * sizeof(perro->owner));
-
-if (perro->owner == NULL)
-{
-free(perro->owner);
+free(new_d);
 return (NULL);
 }
-for (i = 0; i < ownerlen; i++)
-perro->owner[i] = owner[i];
-return (perro);
+for (i = 0; owner[i] != '\0'; i++)
+;
+new_d->owner = malloc(sizeof(char) * i + 1);
+if (new_d->owner == NULL)
+{
+free(new_d->name);
+free(new_d);
+return (NULL);
+}
+for (i = 0; name[i] != '\0'; i++)
+new_d->name[i] = name[i];
+new_d->name[i] = '\0';
+for (i = 0; owner[i] != '\0'; i++)
+new_d->owner[i] = owner[i];
+new_d->owner[i] = '\0';
+new_d->age = age;
+return (new_d);
 }

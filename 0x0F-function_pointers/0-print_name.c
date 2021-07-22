9302f5206067a8da3393baf4e1420026441aca02
root <root@Portatil.localdomain>
2021-07-21 21:33:15 -0500

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #inclide "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name
+ * print_name - prints a name
  * @name: name of the person
- * *f: pointer to function
+ * @f: pointer to function
  */
 void print_name(char *name, void (*f)(char *))
 {

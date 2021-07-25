2c33b7b7cda13d83084d0223c18556d1e4a112c2
SandraHoyos <slhoyosr@gmail.com>
2021-07-25 12:44:06 -0500

    Fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,18 +9,30 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int aux;
+int num1, num2, result;
+int (*res)(int, int);
 
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-
-aux = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-printf("%d\n", aux);
-
+if (argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+res = get_op_func(argv[2]);
+if (res == 0)
+{
+printf("Error\n");
+exit(99);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+result = res(num1, num2);
+printf("%d\n", result);
 return (0);
 }

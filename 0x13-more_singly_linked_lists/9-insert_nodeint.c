8b7f0f52278bf6a2647d37ef0a94b930535b3bb9
SandraHoyos <slhoyosr@gmail.com>
2021-08-04 18:20:28 -0500

    Insertar

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
-/**
-* @head: pointer to the pointer to list
-* @idx: given index to insert node at
-* @n: data to be inserted
-* Return: the address of new node inserted or NULL
-*/
+ /**
+  * insert_nodeint_at_index - function to insert
+  * @head: pointer to the pointer to list
+  * @idx: given index to insert node at
+  * @n: data to be inserted
+  * Return: the address of new node inserted or NULL
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new, *old, *temp;

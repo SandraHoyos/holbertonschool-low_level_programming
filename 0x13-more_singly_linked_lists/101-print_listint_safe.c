38a4df98a2295ea5b71a03e3d928e57d957f3506
SandraHoyos <slhoyosr@gmail.com>
2021-08-04 23:10:39 -0500

    print list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print list with a loop
+ * @head: pointer to the list
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+const listint_t *new, *comp;
+size_t count_nuevo = 0, count_comp = 0;
+
+nuevo = head;
+comp = head;
+while (nuevo != NULL)
+{
+while (count_nuevo > count_comp)
+{
+if (nuevo == comp)
+{
+printf("-> [%p] %d\n", (void *)nuevo, nuevo->n);
+return (count_nuevo);
+}
+count_comp++;
+comp = comp->next;
+}
+comp = head;
+count_comp = 0;
+printf("[%p] %d\n", (void *)nuevo, nuevo->n);
+count_nuevo++;
+nuevo = nuevo->next;
+}
+return (count_nuevo);
+}

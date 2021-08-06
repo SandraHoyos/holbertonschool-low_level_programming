a36b9f4cdff2523661b3e15b259592f08b76730b
SandraHoyos <slhoyosr@gmail.com>
2021-08-05 22:10:05 -0500

    fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,31 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  unsigned int n;
-
-  n = binary_to_uint("1");
-  printf("%u\n", n);
-  n = binary_to_uint("101");
-  printf("%u\n", n);
-  n = binary_to_uint("1e01");
-  printf("%u\n", n);
-  n = binary_to_uint("1100010");
-  printf("%u\n", n);
-  n = binary_to_uint("0000000000000000000110010010");
-  printf("%u\n", n);
-  return (0);
-}
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(char c);
+
+#endif

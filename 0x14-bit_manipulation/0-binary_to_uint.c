208dc22cc446100c67807aa7b2bf464ec9c955ed
SandraHoyos <slhoyosr@gmail.com>
2021-08-05 22:04:11 -0500

    binario a unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,27 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- *binary_to_uint - function that converts a binary number
- *@b: pointer
- *Return: j
+ * binary_to_uint - function to convert binary to unsigned integer
+ * @b: binary number being passed
+ * Return: 0 if not b is not binary else converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int j = 0;
-int i = 0;
-int convert = 1;
+unsigned int result = 0;
+int a = 0, c = 0;
 
-if (b == NULL)
+if (b == NULL || (*b != '0' && *b != '1'))
 return (0);
-
-while (b[i + 1])
+for (a = 0; b[a] != '\0'; a++)
+;
+a--;
+while (a >= 0)
 {
-if (b[i] != '0' && b[i] != '1')
-{
+if (b[a] != '0' && b[a] != '1')
 return (0);
+result = result + (b[a] - '0') * (1 << c);
+a--;
+c++;
 }
-i++;
+return (result);
 }
-while (i >= 0)
-{
-j = j + ((b[i] - '0') * convert);
-unsigned int j = 0;
-int i = 0;
-int convert = 1;
-if (b == NULL)
-return (0);
-while (b[i + 1])
-{
-if (b[i] != '0' && b[i] != '1')
-{
-return (0);
-}
-i++;
-}
-while (i >= 0)
-{
-j = j + ((b[i] - '0') * convert);
-convert = convert * 2;
-i--;
-}
-return (j);

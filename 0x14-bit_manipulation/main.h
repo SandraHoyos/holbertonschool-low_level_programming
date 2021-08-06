2e05a70ea0ff9a97ee30decf89cc00179f1fc0b5
SandraHoyos <slhoyosr@gmail.com>
2021-08-05 22:07:14 -0500

    fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -22,7 +22,7 @@ int main(void)
   printf("%u\n", n);
   return (0);
 }
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);

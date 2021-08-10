610251f32984dd624ef1f47d68fc593bd314351c
SandraHoyos <slhoyosr@gmail.com>
2021-08-09 23:22:54 -0500

    IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,28 @@
 #include "main.h"
 /**
  * read_textfile - Function that reads a file and prints it
- * filename: ptr to the file to read and print
+ * @filename: ptr to the file to read and print
  * @letters: number of bytes to print
  * Return: 0 else number of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* declare pointer to allocate the lenght */
+
 	char *buf;
 	ssize_t rd, op;
 
 	buf = malloc(sizeof(char) * letters);
 
 	if (buf == NULL || filename == NULL)
-	{
-		return (0);
-	}
-	/* open file for read only assigned to var open */
+	return (0);
+
 	op = open(filename, O_RDONLY);
-	/* function read assigned to var */
+
 	rd = read(op, buf, letters);
 	write(STDOUT_FILENO, buf, rd);
-
 	if (op == -1)
-	{
-		return (0);
-	}
-	free (buf);
-	close (op);
+	return (0);
+	free(buf);
+	close(op);
 	return (rd);
 }
